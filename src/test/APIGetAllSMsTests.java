e0b5b21ac52772ba4536a760ce3a650894651dac
Sai <Sai@example.com>
2019-07-24 17:57:11 +0530

    first commit

--- src/test/APIGetAllSMsTests.java
+++ src/test/APIGetAllSMsTests.java
@@ -2,227 +2,200 @@ package test;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
- 
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class APIEumsGETTests {
+public class APIGetAllSMsTests {
 	
-	@Test
-	public void GetEUMsStandardRole() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
+  @Test
+  public void GetAllSMsInvalidAPIToken() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
 		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
+		RestAssured.baseURI = "https://localhost:8445/v2/sms";
 
 		// Get the RequestSpecification of the request that you want to sent
 		// to the server. The server is specified by the BaseURI that we have
 		// specified in the above step.
 		RequestSpecification httpRequest = RestAssured.given();
 		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIx");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=89");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 200,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsInvalidName() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eumsa";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIx");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=53");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 405,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsSuperAdminRole() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIz");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=1027");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 200,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsInvalidPageNumber() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIz");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=100");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 400,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsCustomerOwnerRole() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXI3");
+		httpRequest.header("Authorization", "indmFsaWQtY3VzdG9tZXIx");
 
 		// Make a request to the server by specifying the method Type and the method URL.
 		// This will return the Response from the server. Store the response in a variable.
 		Response response = httpRequest.request(Method.GET, "?page=1");
 
 		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 200,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsAdminRole() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXI3");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=1");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 200,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsInvalidAPIToken() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		httpRequest.header("Authorization", "invalid_customer1");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=1");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
-		String responseBody = response.getBody().asString();
-		int statusCode = response.getStatusCode();
-		System.out.println("Response Body is =>  " + responseBody);
-		System.out.println("Status Code is => " + statusCode);
-		Assert.assertEquals(statusCode, 200,"Correct status code");
-	}
-	
-	@Test
-	public void GetEUMsMissingAPIToken() {
-		
-		RestAssured.useRelaxedHTTPSValidation();
-		// Specify the base URL to the RESTful web service
-		RestAssured.baseURI = "https://localhost:8445/v2/eums";
-
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
-		RequestSpecification httpRequest = RestAssured.given();
-		httpRequest.header("Content-Type", "application/json");
-		//httpRequest.header("Authorization", "invalid_customer1");
-
-		// Make a request to the server by specifying the method Type and the method URL.
-		// This will return the Response from the server. Store the response in a variable.
-		Response response = httpRequest.request(Method.GET, "?page=1");
-
-		// Now let us print the body of the message to see what response
-		// we have received from the server
+		// we have recieved from the server
 		String responseBody = response.getBody().asString();
 		int statusCode = response.getStatusCode();
 		System.out.println("Response Body is =>  " + responseBody);
 		System.out.println("Status Code is => " + statusCode);
 		Assert.assertEquals(statusCode, 401,"Correct status code");
-	}
+  }
+  
+  @Test
+  public void GetAllSMs() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://localhost:8445/v2/sms";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIx");
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "?page=1");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		System.out.println("Response Body is =>  " + responseBody);
+		System.out.println("Status Code is => " + statusCode);
+		Assert.assertEquals(statusCode, 200,"Correct status code");
+  }
+  
+  @Test
+  public void GetAllSMsSuperAdmin() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://localhost:8445/v2/sms";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIz");
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "?page=1");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		System.out.println("Response Body is =>  " + responseBody);
+		System.out.println("Status Code is => " + statusCode);
+		Assert.assertEquals(statusCode, 200,"Correct status code");
+  }
+  
+  @Test
+  public void GetAllSMsCustomerOwnerRole() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://localhost:8445/v2/sms";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXI3");
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "?page=1");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		System.out.println("Response Body is =>  " + responseBody);
+		System.out.println("Status Code is => " + statusCode);
+		Assert.assertEquals(statusCode, 200,"Correct status code");
+  }
+  
+  @Test(enabled=false)
+  public void GetAllSMsMissingAPIToken() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://localhost:8445/v2/sms";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		//httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXI3");
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		System.out.println("Response Body is =>  " + responseBody);
+		System.out.println("Status Code is => " + statusCode);
+		Assert.assertEquals(statusCode, 401,"Correct status code");
+  }
+  
+  @Test
+  public void GetAllSMsInvalidParamName() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://localhost:8445/v2/SMS";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXIx");
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		System.out.println("Response Body is =>  " + responseBody);
+		System.out.println("Status Code is => " + statusCode);
+		Assert.assertEquals(statusCode, 404,"Correct status code");
+  }
+  
+  @Test
+  public void GetAllSMsAdminRole() {
+	  
+	  RestAssured.useRelaxedHTTPSValidation();
+		// Specify the base URL to the RESTful web service
+		RestAssured.baseURI = "https://localhost:8445/v2/sms";
+
+		// Get the RequestSpecification of the request that you want to sent
+		// to the server. The server is specified by the BaseURI that we have
+		// specified in the above step.
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		httpRequest.header("Authorization", "dmFsaWQtY3VzdG9tZXI1");
+
+		// Make a request to the server by specifying the method Type and the method URL.
+		// This will return the Response from the server. Store the response in a variable.
+		Response response = httpRequest.request(Method.GET, "?page=1");
+
+		// Now let us print the body of the message to see what response
+		// we have recieved from the server
+		String responseBody = response.getBody().asString();
+		int statusCode = response.getStatusCode();
+		System.out.println("Response Body is =>  " + responseBody);
+		System.out.println("Status Code is => " + statusCode);
+		Assert.assertEquals(statusCode, 200,"Correct status code");
+  }
 }
